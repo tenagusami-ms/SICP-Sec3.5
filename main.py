2216c47583ef6bc5dd909dec3dd31ee75d2400e6
KAN-YA, Yukitoshi <tenagusami@hotmail.com>
2021-09-25 15:46:25 +0900

    feat: until exercise 3.60

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ main module
 """
 from __future__ import annotations
 
-from itertools import takewhile
+from itertools import takewhile, islice, repeat, chain
 from typing import Sequence, Generator, Any
 
 from modules.Math import is_prime
 from modules.Stream import integers_starting_from, integers, is_divisible, stream_reference, fibonacci_generator, \
-    eratosthenes_sieve, integers_from_ones, fibonacci_adding, double
+    eratosthenes_sieve, integers_from_ones, fibonacci_adding, double, factorial, partial_sums, humming_stream, expand, \
+    exponential, sine, cosine, multiply_series, add_series, tangent, inverted_unit_series
 
 
 def main() -> None:
@@ -37,6 +38,26 @@ def main() -> None:
 
     doubles: Generator[int, None, Any] = double()
     print(list(takewhile(lambda x: x < 100, doubles)))
+
+    print(f"exercise 3.54: factorial = {list(takewhile(lambda x: x < 1000, factorial()))}")
+
+    print(f"exercise 3.55: triangular = {list(takewhile(lambda x: x < 100, partial_sums(integers_starting_from(1))))}")
+
+    print(f"exercise 3.56: Humming = {list(takewhile(lambda x: x < 100, humming_stream()))}")
+
+    print(f"exercise 3.58: (expand 1 7 10) = {list(islice(expand(1, 7, 10), 10))}")
+    print(f"exercise 3.58: (expand 3 8 10) = {list(islice(expand(3, 8, 10), 10))}")
+
+    print(f"exercise 3.59b: (exp-series) = {list(islice(exponential(), 10))}")
+    print(f"exercise 3.59b: (sine-series) = {list(islice(sine(), 10))}")
+    print(f"exercise 3.59b: (cosine-series) = {list(islice(cosine(), 10))}")
+
+    print(f"exercise 3.60: (sin^2 + cos^2) ="
+          f" {list(islice(add_series(multiply_series(cosine(), cosine()), multiply_series(sine(), sine())), 10))}")
+
+    print(f"exercise 3.61: (secant-series) = {list(islice(inverted_unit_series(chain([1.0, -1.0], repeat(0.0))), 3))}")
+    # print(f"exercise 3.61: (secant-series) = {list(islice(inverted_unit_series(cosine()), 5))}")
+    # print(f"exercise 3.61: (tangent-series) = {list(islice(tangent(), 2))}")
 
 
 if __name__ == '__main__':
--- modules/Stream.py
+++ modules/Stream.py
@@ -3,13 +3,15 @@ stream module
 """
 from __future__ import annotations
 
-from itertools import count, takewhile, repeat
-from typing import Iterable, TypeVar, Any, Generator
+import sys
+from itertools import count, takewhile, repeat, accumulate, chain
+from typing import Iterable, TypeVar, Any, Generator, Iterator, Callable
 
 T = TypeVar("T")
+sys.setrecursionlimit(1000)
 
 
-def integers_starting_from(n: int) -> Iterable[int]:
+def integers_starting_from(n: int) -> Iterator[int]:
     """
     infinite stream of numbers from
     Args:
@@ -20,7 +22,7 @@ def integers_starting_from(n: int) -> Iterable[int]:
     return count(n)
 
 
-def integers() -> Iterable[int]:
+def integers() -> Iterator[int]:
     """
     integers
     Returns:
@@ -131,3 +133,173 @@ def double() -> Generator[int, None, Any]:
     yield from (2 * i for i in double())
 
 
+def multiply_streams(s1: Iterable[T], s2: Iterable[T]) -> Iterator[T]:
+    """
+    exercise 3.54-1
+    """
+    yield next(iter(s1)) * next(iter(s2))
+    yield from multiply_streams(s1, s2)
+
+
+def add_2streams(s1: Iterable[T], s2: Iterable[T]) -> Iterator[T]:
+    """
+    exercise 3.54-1
+    """
+    yield next(iter(s1)) + next(iter(s2))
+    yield from multiply_streams(s1, s2)
+
+
+def add_streams(*streams) -> Iterator[T]:
+    """
+    multiple addition
+    """
+    if len(streams) < 2:
+        yield from streams[0]
+    yield from add_2streams(streams[0], add_streams(*streams[1:]))
+
+
+def factorial() -> Generator[int, None, Any]:
+    """
+    exercise 3.54-2
+    """
+    yield 1
+    yield from multiply_streams(integers_starting_from(2), factorial())
+
+
+def partial_sums(s: Iterator[int]) -> Iterator[int]:
+    """
+    exercise 3.55
+    """
+    return accumulate(s)
+
+
+def scale_streams(s: Iterator[T], factor: T) -> Iterator[T]:
+    """
+    scale streams
+    """
+    yield next(iter(s)) * factor
+    yield from scale_streams(s, factor)
+
+
+def merge(s1: Iterator[int], s2: Iterator[int]) -> Iterator[int]:
+    """
+    exercise 3.56-1
+    """
+    # try:
+    #     v1: int = next(iter(s1))
+    # except StopIteration:
+    #     yield from s2
+    #
+    # try:
+    #     v2: int = next(iter(s2))
+    # except StopIteration:
+    #     yield from s1
+    v1: int = next(iter(s1))
+    v2: int = next(iter(s2))
+
+    if v1 < v2:
+        yield v1
+        yield from merge(s1, chain([v2], s2))
+    elif v1 > v2:
+        yield v2
+        yield from merge(chain([v1], s1), s2)
+    else:
+        yield v1
+        yield from merge(s1, s2)
+
+
+def humming_stream() -> Iterator[int]:
+    """
+    exercise 3.56-2
+    """
+    yield 1
+    yield from merge(scale_streams(humming_stream(), 2),
+                     merge(scale_streams(humming_stream(), 3), scale_streams(humming_stream(), 5)))
+
+
+def expand(numerator: int, denominator: int, radix: int) -> Iterator[int]:
+    """
+    exercise 3.58
+    """
+    yield (numerator * radix) // denominator
+    yield from expand((numerator * radix) % denominator, denominator, radix)
+
+
+def integrate_series(integration_constant: float, s: Iterator[float]) -> Iterator[float]:
+    """
+    exercise 3.59a
+    """
+    yield integration_constant
+    coefficient_inverse: float = 0.0
+    while True:
+        coefficient_inverse += 1.0
+        yield next(iter(s)) / coefficient_inverse
+
+
+def negate_series(s: Iterator[float]) -> Iterator[float]:
+    """
+    negate
+    """
+    yield from multiply_streams(s, repeat(-1.0))
+
+
+def exponential() -> Iterator[float]:
+    """
+    exercise 3.59b-1
+    """
+    yield from integrate_series(1.0, exponential())
+
+
+def sine() -> Iterator[float]:
+    """
+    exercise 3.59b-2
+    """
+    yield from integrate_series(0.0, (integrate_series(1.0, negate_series(sine()))))
+
+
+def cosine() -> Iterator[float]:
+    """
+    exercise 3.59b-2
+    """
+    yield from integrate_series(1.0, (integrate_series(0.0, negate_series(cosine()))))
+
+
+add_series: Callable[[Iterator[T], ...], Iterator[T]] = add_streams
+
+
+def multiply_series(s0: Iterator[float], s1: Iterator[float]) -> Iterator[float]:
+    """
+    exercise 3.60
+    """
+    v0: float = next(iter(s0))
+    v1: float = next(iter(s1))
+    yield v0 * v1
+    yield from add_series(scale_streams(s1, v0), scale_streams(s0, v1), multiply_streams(s0, s1))
+
+
+def inverted_unit_series(s: Iterator[float]) -> Iterator[float]:
+    """
+    exercise 3.61-1
+    """
+    first: float = next(iter(s))
+    yield first
+    yield from multiply_series(negate_series(s), inverted_unit_series(chain([first], s)))
+
+
+def divide_series(numerator: Iterator[float], denominator: Iterator[float]) -> Iterator[float]:
+    """
+    exercise 3.61-2
+    """
+    denominator_first_coefficient: float = next(iter(denominator))
+    if denominator_first_coefficient == 0.0:
+        raise ValueError("division must be done by the series with non-zero 0th-order term.")
+    yield from multiply_series(
+        scale_streams(numerator, 1.0 / denominator_first_coefficient),
+        inverted_unit_series(chain([1.0], scale_streams(denominator, 1.0 / denominator_first_coefficient))))
+
+
+def tangent() -> Iterator[float]:
+    """
+    exercise 3.61-3
+    """
+    yield from divide_series(sine(), cosine())
